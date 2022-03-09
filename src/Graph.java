f60ee91f4ec6fed9b006fffc02cd034abf7871bc
Jérôme Devaux <jerome.devaux@student.vinci.be>
2022-03-09 11:58:26 +0100

    read file and extract an airport

--- src/Graph.java
+++ src/Graph.java
@@ -1,12 +1,35 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Graph {
   File aeroports;
   File vols;
 
-  public Graph(File aeroports, File vols) {
+
+  public Graph(File aeroports, File vols) throws FileNotFoundException {
     this.aeroports = aeroports;
     this.vols = vols;
+    try{
+      BufferedReader objReaderAeroport = new BufferedReader(new FileReader(aeroports.toString()));
+      BufferedReader objReaderVols = new BufferedReader(new FileReader(vols.toString()));
+      String strCurrentLine;
+      String[] maLigneAeroport;
+
+      while ((strCurrentLine = objReaderAeroport.readLine()) != null) {
+        maLigneAeroport = strCurrentLine.split(",", 6);
+        Aeroport nouveauAeroport = new Aeroport(maLigneAeroport[0],maLigneAeroport[1],maLigneAeroport[2],maLigneAeroport[3],
+            Double.parseDouble(maLigneAeroport[4]),Double.parseDouble(maLigneAeroport[5]));
+
+      }
+    }catch (Exception e){
+      throw new FileNotFoundException();
+    }
+
+
   }
 
  void calculerItineraireMinimisantNombreVol(String src , String dest){
