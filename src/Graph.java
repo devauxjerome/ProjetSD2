29ec166eb84ab9f1375e865ad64668618b27c0c7
Jérôme Devaux <jerome.devaux@student.vinci.be>
2022-03-09 11:13:39 +0100

    init graph

--- src/Graph.java
+++ src/Graph.java
@@ -1,3 +1,19 @@
-public class Graph {
+import java.io.File;
 
+public class Graph {
+  File aeroports;
+  File vols;
+
+  public Graph(File aeroports, File vols) {
+    this.aeroports = aeroports;
+    this.vols = vols;
+  }
+
+ void calculerItineraireMinimisantNombreVol(String src , String dest){
+
+ }
+
+ void calculerItineraireMiniminantDistance(String src, String dest){
+
+ }
 }
