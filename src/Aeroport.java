7c229dc68da02f42c213fb4be1509eabd96d3755
Jérôme Devaux <jerome.devaux@student.vinci.be>
2022-03-09 11:58:46 +0100

    read file and extract an airport

---
+++ src/Aeroport.java
@@ -0,0 +1,54 @@
+public class Aeroport {
+  String codeIATA;
+  String name;
+  String ville;
+  String pays;
+  double longitude;
+  double latitude;
+
+  public Aeroport(String codeIATA, String name, String ville, String pays, double longitude,
+      double latitude) {
+    this.codeIATA = codeIATA;
+    this.name = name;
+    this.ville = ville;
+    this.pays = pays;
+    this.longitude = longitude;
+    this.latitude = latitude;
+  }
+
+  public String getCodeIATA() {
+    return codeIATA;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getVille() {
+    return ville;
+  }
+
+  public String getPays() {
+    return pays;
+  }
+
+  public double getLongitude() {
+    return longitude;
+  }
+
+  public double getLatitude() {
+    return latitude;
+  }
+
+  @Override
+  public String toString() {
+    return "Aeroport{" +
+        "codeIATA='" + codeIATA + '\'' +
+        ", name='" + name + '\'' +
+        ", ville='" + ville + '\'' +
+        ", pays='" + pays + '\'' +
+        ", longitude=" + longitude +
+        ", latitude=" + latitude +
+        '}';
+  }
+}
